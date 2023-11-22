b0802ecc710279ac66d141482425cda364f711bc
xuzhenxing <xuzhenxing.coder@bytedance.com>
2023-11-22 22:24:28 +0800

    fix: docker compose up should use -d

--- create_network.sh
+++ create_network.sh
@@ -1,22 +1,22 @@
 env_file="dev_env"
-#env_file="deploy_env"
-network_name="your_network_name"
+# env_file="deploy_env"
+network_name="q_data_proj_pg_net"
 
 if docker network inspect "$network_name" >/dev/null 2>&1; then
     echo "The network $network_name exists."
 else
     echo "The network $network_name does not exist, creating it!"
+    docker network create $network_name
 fi
-docker network create $network_name
 
 echo "up db...."
-docker-compose -f db_compose.yml -p db --env-file ${env_file} up
+docker-compose -f db_compose.yml -p db --env-file ${env_file}  up -d
 echo "db running...."
 echo "stop command: docker-compose -f db_compose.yml -p db down"
 
 echo "up tools..."
 docker exec db-postgres-1 psql -d postgres -c "CREATE DATABASE prefect"
 
-docker-compose -f tools_compose.yml --env-file ${env_file} up
+docker-compose -f tools_compose.yml --env-file ${env_file} up -d
 echo "tools running..."
 echo "stop command: docker-compose -f tools_compose.yml --env-file ${env_file} up"
